9e3c20b744846868f366fa172ae436f2e3071d23
Devin Gunay <devingunay@gmail.com>
2019-02-11 22:22:48 -0800

    Minor refactors and add WSL symlinking

--- setup_environment.pl
+++ setup_environment.pl
@@ -3,14 +3,27 @@
 
 use File::Copy;
 use File::Basename;
+use English;
+
+die "Script only works in Linux/WSL" unless $OSNAME eq 'linux';
 
 # Environment stuff
 my $bin_dir = $ENV{'HOME'} . '/.local/bin';
 
+##############
 # CLI args
-my $dry_run = grep { /^(?:-d|--dry-run)$/ } @ARGV;
+##############
+# Global, determines whether any changes actually happen
+my $dry_run = grep { /^(?:-d|--dry-run)$/ } @ARGV; 
+
+# If flag is set, does not prompt before overwriting a file
 my $overwrite_all = grep { /^(?:-o|--overwrite-all)$/ } @ARGV;
 
+# See later for -w|--winhome arg
+
+##############
+# Script
+##############
 # Copy .vimrc into home
 print "Copying .vimrc to ~/.vimrc...\n";
 install_vimrc();
@@ -19,41 +32,67 @@ install_vimrc();
 print "Installing tldr...\n";
 install_tealdeer($bin_dir);
 
-# Detect if WSL
+# Detect if WSL or exit
 my $wsl = grep { /Microsoft/i } `uname -a`;
 exit 0 unless $wsl;
 print "Windows Subsystem for Linux detected.\n";
 
 # Symlink Documents, Music, Videos, etc to home
 my $home = $ENV{'HOME'};
-my $winhome = ''; # TODO: figure out how to get this or ask the user
-symlink_windows_folders_to_home($winhome);
 
+# Find the user-provided windows home dir
+my $winhome = 0; 
+for (@ARGV) {
+  $winhome = $1 if $_ =~ /-w=(.+)$/;
+  $winhome = $1 if $_ =~ /--winhome=(.+)$/;
+  last if $winhome;
+}
+die "No Windows home provided (-w|--winhome=/mnt/c/Users/...)" unless $winhome;
+die "$winhome is not a directory or doesn't exist" unless -d $winhome;
 
-sub symlink_windows_folders_to_home {
-  my $winhome = shift;
+symlink_windows_libraries_to_home($winhome, $home);
 
-  return unless $dry_run;
+exit 0;
 
-  symlink();
+################
+# Subroutines
+################
+sub symlink_windows_libraries_to_home {
+  my $winhome  = shift;
+  my $wsl_home = shift;
+
+  # rtrim slashes from winhome
+  $winhome =~ s/\/+$//g;
+
+  my @libraries = qw(Documents Music Pictures Videos);
+  foreach my $lib (@libraries) {
+    die "$winhome/$lib does not exist" unless -e "$winhome/$lib";
+
+    next unless (
+      $overwrite_all
+      or user_says_yes_to("Symlink $winhome/$lib to $wsl_home/$lib? (y/n)")
+    );
+
+    print "Symlinking $winhome/$lib to $wsl_home/$lib...\n";
+    symlink("$winhome/$lib", "$wsl_home/$lib") unless $dry_run;
+  }
 }
 
 sub install_vimrc {
   # If .vimrc is already there, prompt the user
   if (-e $ENV{'HOME'} . '/.vimrc' and not $overwrite_all) {
-    print "~/.vimrc found. Overwrite? (y/n)";
-
-    if (user_says_yes()) {
-      print "Overwriting ~/.vimrc...\n";
-      copy(dirname(__FILE__) . '/.vimrc', $ENV{'HOME'} . '/.vimrc') unless $dry_run;
-      return;
-    }
+    return unless user_says_yes_to("~/.vimrc found. Overwrite? (y/n)");
+    print "Overwriting ~/.vimrc...\n";
   }
 
   copy(dirname(__FILE__) . '/.vimrc', $ENV{'HOME'} . '/.vimrc') unless $dry_run;
 }
 
-sub user_says_yes {
+sub user_says_yes_to {
+  my $prompt = shift;
+  die unless $prompt;
+
+  print $prompt;
   my $response = <STDIN>;
   chomp $response;
   return $response =~ /y/i;
@@ -65,11 +104,11 @@ sub install_tealdeer {
   die "$bin_dir doesn't exist or is not a directory" unless -d $bin_dir;
 
   if (-e "$bin_dir/tldr") {
-    print "tldr binary already present at $bin_dir/tldr. Overwrite? (y/n)";
-    return 0 unless user_says_yes();
+    return 0 unless user_says_yes_to(
+      "tldr binary already present at $bin_dir/tldr. Overwrite? (y/n)"
+    );
   }
 
-  # Return early if dry run
   return 0 if $dry_run;
 
   # Download the binary (v1.1.0) using wget
